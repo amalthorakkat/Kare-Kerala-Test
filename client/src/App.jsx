396e40c8a60258b0406fb067661781c50fe0a3d7
amalthorakkat <amalthorakkat@gmail.com>
2026-05-14 19:12:51 +0530

    Fixed Header Issue!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import UserLayout from "./components/Layout/UserLayout";
 import Home from "./components/Home/Home";
 import Tourism from "./components/Tourism/Tourism";
 import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <div>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />
